b881e93fbaa3a176db4459cd2f7ff3b95f6e63a5
kvitee <kvitee41@gmail.com>
2026-04-26 18:26:11 +0800

    [adapters] Add SliceExtraAdapters trait implemented for slices.

--- src/lib.rs
+++ src/lib.rs
@@ -43,4 +43,58 @@ pub trait ExtraAdapters: Iterator {
     }
 }
 
+pub trait SliceExtraAdapters<'s, T: Copy>: AsRef<[T]> {
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use extra_adapters::*;
+    /// #
+    /// let data = vec![1, 2, 3];
+    /// let permutations = data.permutations().collect();
+    ///
+    /// assert_eq!(permutations, vec![
+    ///     vec![1, 2, 3],
+    ///     vec![1, 3, 2],
+    ///     vec![2, 1, 3],
+    ///     vec![2, 3, 1],
+    ///     vec![3, 1, 2],
+    ///     vec![3, 2, 1],
+    /// ]);
+    /// ```
+    fn permutations(&'s self) -> Permutations<'s, T> {
+        Permutations::new(self.as_ref(), None)
+    }
+
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use extra_adapters::*;
+    /// #
+    /// let data = vec![1, 2, 3, 4];
+    /// let permutations = data.k_permutations(2).collect();
+    ///
+    /// assert_eq!(permutations, vec![
+    ///     vec![1, 2],
+    ///     vec![1, 3],
+    ///     vec![1, 4],
+    ///     vec![2, 1],
+    ///     vec![2, 3],
+    ///     vec![2, 4],
+    ///     vec![3, 1],
+    ///     vec![3, 2],
+    ///     vec![3, 4],
+    ///     vec![4, 1],
+    ///     vec![4, 2],
+    ///     vec![4, 3],
+    /// ]);
+    /// ```
+    fn k_permutations(&'s self, k: usize) -> Permutations<'s, T> {
+        Permutations::new(self.as_ref(), Some(k))
+    }
+}
+
 impl<I: Iterator> ExtraAdapters for I {}
+
+impl<'s, T: Copy, S: AsRef<[T]>> SliceExtraAdapters<'s, T> for S {}
